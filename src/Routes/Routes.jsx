2a7e727e6c768fca7c9c5ba4bd617f00befa019c
iamanowarbd <iamanowarbd@gmail.com>
2025-10-08 13:26:01 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+
+/** named export **/
+
+export const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <App />,
+  },
+])
+
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router'
+import { router } from './Routes/Routes.jsx'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    {/* Declarative Mode */}
+    {/* <BrowserRouter>
+      <Routes>
+        <Route path='/secret' element={<App/>} />
+
+      </Routes>
+    </BrowserRouter> */}
+    {/* Data Mode */}
+    <RouterProvider router = {router}/>
+  </StrictMode>,
+)
